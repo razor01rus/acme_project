062efe5a6468945b6d382e81cf39eb22745ee9d3
Aleksandr Zinovyev <razor01rus@gmail.com>
2025-01-06 17:37:48 +0300

    Added validation for forms and models.

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,7 @@
-from django.contrib import admin
+from django.contrib import admin # type: ignore
+
+# Из модуля models импортируем модель Category...
+from .models import Birthday
+
+# ...и регистрируем её в админке:
+admin.site.register(Birthday) 
